8ebd6e3fe276081c55661de7b5e6b95e0825c125
Tom Sartain <tomsartain@gmail.com>
2025-01-17 16:37:09 -0500

    Make it a proper Symfony bundle

---
+++ src/BundleRouteBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xiian\BundleRoute;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class BundleRouteBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/../config/services.yaml');
+    }
+
+}
